7e149296b57e3db4fe82df495f53f81e08596fb8
skywang329 <sky.wang@yahoo.com>
2017-07-25 23:53:06 +0800

    init

---
+++ Seer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Seer
+//
+//  Created by Sky Wang on 7/25/17.
+//  Copyright © 2017 Sky Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Seer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Seer
+//
+//  Created by Sky Wang on 7/25/17.
+//  Copyright © 2017 Sky Wang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
